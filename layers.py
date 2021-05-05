56ecafd226b292335750cab34d3e01d8e6c74daf
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-05-05 08:55:29 -0400

    Update layers.py

--- layers.py
+++ layers.py
@@ -15,20 +15,15 @@ class GraphConvolution(Module):
     """
 
     def __init__(self, in_features, out_features, bias=True, output_layer = 0):
-        super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.output_layer = output_layer
         self.weight_low = Parameter(torch.FloatTensor(in_features, out_features))
-        #self.weight_high = Parameter(torch.FloatTensor(in_features, out_features))
-        #self.weight_mlp = Parameter(torch.FloatTensor(in_features, out_features))
-        #self.weight = Parameter(torch.FloatTensor(out_features, out_features))
-        #self.att_trans = Parameter(torch.FloatTensor(in_features, out_features))
-        #self.att_trans_high = Parameter(torch.FloatTensor(out_features, 1))
-        #self.att_trans_mlp = Parameter(torch.FloatTensor(out_features, 1))
-        #self.att_vec = Parameter(torch.FloatTensor(out_features, 1))
-        #self.I = (torch.tensor(0.5))
-        #self.P = (torch.tensor(0.5))
+        self.weight_high = Parameter(torch.FloatTensor(in_features, out_features))
+        self.weight_mlp = Parameter(torch.FloatTensor(in_features, out_features))
+        self.att_vec_mlp= nn.Linear(out_features, 1, bias=bias)
+        self.att_vec_low = nn.Linear(out_features, 1, bias=bias)
+        self.att_vec_high = nn.Linear(out_features, 1, bias=bias)
         
         
         if bias:
@@ -38,57 +33,54 @@ class GraphConvolution(Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        gain = nn.init.calculate_gain('relu')
         stdv = 1. / math.sqrt(self.weight_low.size(1))
         self.weight_low.data.uniform_(-stdv, stdv)
-        #self.weight_high.data.uniform_(-stdv, stdv)
-        #self.weight_mlp.data.uniform_(-stdv, stdv)
-        #self.weight.data.uniform_(-stdv, stdv)
+        self.weight_high.data.uniform_(-stdv, stdv)
+        self.weight_mlp.data.uniform_(-stdv, stdv)
+        nn.init.xavier_uniform_(self.att_vec_mlp.weight, gain=gain)
+        nn.init.xavier_uniform_(self.att_vec_low.weight, gain=gain)
+        nn.init.xavier_uniform_(self.att_vec_high.weight, gain=gain)
         
-        #self.att_trans.data.uniform_(-stdv, stdv)
-        #self.att_trans_high.data.uniform_(-stdv, stdv)
-        #self.att_trans_mlp.data.uniform_(-stdv, stdv)
-        #self.att_vec.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
             self.bias_low.data.uniform_(-stdv, stdv)
             self.bias_high.data.uniform_(-stdv, stdv)
             self.bias_mlp.data.uniform_(-stdv, stdv)
+                
+    def attention(self, output_low, output_high, output_mlp):
+        low_norm = (torch.norm(output_low,dim=1).detach())[:,None] + 1e-16#.detach()
+        high_norm = (torch.norm(output_high,dim=1).detach())[:,None]+ 1e-16 #.detach()
+        mlp_norm = (torch.norm(output_mlp,dim=1).detach())[:,None] + 1e-16#.detach() torch.norm((support_mlp).detach(),dim=1) +
+        # T_norm = low_norm + high_norm + mlp_norm + 1e-16
+        
+        
+        att_mlp = F.elu(self.att_vec_mlp(torch.cat([output_mlp.detach()/mlp_norm],1)),alpha = 5)
+        att_low = F.elu(self.att_vec_low(torch.cat([output_low.detach()/low_norm],1)),alpha = 5)
+        att_high = F.elu(self.att_vec_high(torch.cat([output_high.detach()/high_norm],1)),alpha = 5)
+ 
+        return torch.sigmoid(att_low),torch.sigmoid(att_high),torch.sigmoid(att_mlp)
 
-    def forward(self, input, adj):
+    def forward(self, input, adj_low, adj_high):
         #adj = adj.to_dense()
         #nnodes = adj.size(1)
-        adj_low = adj #self.I * torch.eye(nnodes) + self.P * adj
+        #adj_low = adj #self.I * torch.eye(nnodes) + self.P * adj
         #adj_high = torch.eye(nnodes) - adj_low
         support_low = torch.mm(input, self.weight_low)
-        #support_low = F.relu(support_low) #+self.bias_low
+        support_low = F.relu(support_low) #+self.bias_low
         output_low = torch.spmm(adj_low, support_low)
         
         
-        #support_high = torch.mm(input, self.weight_high)
-        #support_high = F.relu(support_high) #+self.bias_high
-        #output_high = torch.spmm(adj_high, support_high)
+        support_high = torch.mm(input, self.weight_high)
+        support_high = F.relu(support_high) #+self.bias_high
+        output_high = torch.spmm(adj_high, support_high)
         
         
-        #output_mlp = F.relu(torch.mm(input, self.weight_mlp)) # +self.bias_mlp
+        output_mlp = F.relu(torch.mm(input, self.weight_mlp)) # +self.bias_mlp
         
-        #Compute Attention
-        #combined_output = F.sigmoid(torch.mm(input, self.att_trans)) #output_mlp + output_low + output_high  #
-        # combined_trans = torch.mm(combined_output,self.att_trans)
-        # low_trans = torch.mm(output_low,self.att_trans)
-        # high_trans = torch.mm(output_high,self.att_trans)
-        # mlp_trans = torch.mm(output_mlp,self.att_trans)
+        att_low, att_high, att_mlp = self.attention(output_low, output_high, output_mlp)
         
-        #att_low_output_trans = F.leaky_relu(torch.mm(torch.cat([combined_output,output_low],1), self.att_trans_low))
-        #att_low_output_trans = F.leaky_relu(torch.mm(torch.cat([combined_output,output_low],1), self.att_trans_low))
-        #att_high_output_trans = F.leaky_relu(torch.mm(output_high, self.att_trans_high))
-        #att_mlp_trans = F.leaky_relu(torch.mm(output_mlp, self.att_trans_mlp))
-        
-        
-        #att_low = F.elu(torch.mm(torch.cat([output_low],1), self.att_vec))
-        #att_high = F.elu(torch.mm(torch.cat([output_high],1), self.att_vec))
-        #att_mlp = F.elu(torch.mm(torch.cat([output_mlp],1), self.att_vec))
-        
-        #att = torch.softmax(torch.cat([att_low,att_high,att_mlp],1),1) #
+        output_low = 3*(att_low*output_low + att_high*output_high + att_mlp*output_mlp)
         
         #output = 3*(att[:,0][:,None]*output_low + att[:,1][:,None]*output_high+ att[:,2][:,None]*output_mlp )# output_mlp   # output_low + output_high + # # + self.c*output+self.c_high*output_high/(self.c+self.c_high)
         # if self.output_layer:
