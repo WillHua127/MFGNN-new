15cfb946fa7321664e0b733fd97e43d646d202ff
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-05-06 08:46:59 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -408,12 +408,12 @@ def full_load_data(dataset_name, sub_dataname=''):
     # test_mask = th.BoolTensor(test_mask)
 
     adj = normalize_adj(adj+sp.eye(adj.shape[0]))
-    adj_high = sp.eye(adj.shape[0]) - adj
+    #adj_high = sp.eye(adj.shape[0]) - adj
     adj = sparse_mx_to_torch_sparse_tensor(adj)
-    adj_high = sparse_mx_to_torch_sparse_tensor(adj_high)
+    #adj_high = sparse_mx_to_torch_sparse_tensor(adj_high)
     #g_high = g
     
-    return adj, adj_high, features, labels #, train_mask, val_mask, test_mask
+    return adj, 0, features, labels #, train_mask, val_mask, test_mask
 
 def reconstruct(g, rank, num_classes):
     nodelist = np.array(sorted(g.nodes()))
