7c92e9d7403f580eefd1faf0b9a508adab3b884b
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-05-17 07:03:46 -0400

    Update train.py

--- train.py
+++ train.py
@@ -244,7 +244,7 @@ def train_sgc():
         result = np.zeros(5)
         t_total = time.time()
         num_epoch = 0
-        for idx in range(5):
+        for idx in range(10):
             #idx_train, idx_val, idx_test = rand_train_test_idx(labels)
             idx_train, idx_val, idx_test = random_disassortative_splits(labels, num_class)
             #rank = OneVsRestClassifier(LinearRegression()).fit(features[idx_train], labels[idx_train]).predict(features)
