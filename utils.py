bfbba5ca899acc9be32f5630c613943f39ab2c67
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-05-19 13:29:10 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -33,7 +33,7 @@ dataset_drive_url = {
 def mfsgc_precompute(features, adj, degree):
     features_low = features
     for i in range(degree):
-        features_low = torch.spmm(adj, features_low)
+        features_low = torch.spmm(adj, features_low.to_dense())
     features_high = features - features_low
     return features_low, features_high 
 
