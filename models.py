1ae6e2f7e3913cfa1bccaeadd9bbab347d5029b8
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-05-23 02:30:21 -0400

    Update models.py

--- models.py
+++ models.py
@@ -4,6 +4,80 @@ from layers import GraphConvolution, SAGEConv
 import torch
 #from torch_geometric.nn import SGConv
 
+
+
+class SAGEBC(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 dropout):
+        super().__init__()
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden
+        self.n_classes = n_classes
+        self.layers = nn.ModuleList()
+        self.layers.append(SAGEConv(in_feats, n_hidden))
+        for i in range(1, n_layers - 1):
+            self.layers.append(SAGEConv(n_hidden, n_hidden))
+        self.layers.append(SAGEConv(n_hidden, n_classes))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, blocks, x):
+        h = x
+        #print(blocks)
+        for l, (layer, block) in enumerate(zip(self.layers, blocks)):
+            #h_dst = h[:block.number_of_dst_nodes()]
+            h = layer(block, h)
+            if l != len(self.layers) - 1:
+                h = F.relu(h)
+                h = self.dropout(h)
+        return h
+    
+    
+    def inference(self, g, x, device, batch_size, num_workers):
+        """
+        Inference with the GraphSAGE model on full neighbors (i.e. without neighbor sampling).
+        g : the entire graph.
+        x : the input of entire node set.
+        The inference code is written in a fashion that it could handle any number of nodes and
+        layers.
+        """
+        # During inference with sampling, multi-layer blocks are very inefficient because
+        # lots of computations in the first few layers are repeated.
+        # Therefore, we compute the representation of all nodes layer by layer.  The nodes
+        # on each layer are of course splitted in batches.
+        # TODO: can we standardize this?
+        for l, layer in enumerate(self.layers):
+            y = torch.zeros(g.num_nodes(), self.n_hidden if l != len(self.layers) - 1 else self.n_classes)
+
+            sampler = dgl.dataloading.MultiLayerFullNeighborSampler(1)
+            dataloader = dgl.dataloading.NodeDataLoader(
+                g,
+                torch.arange(g.num_nodes()).to(g.device),
+                sampler,
+                batch_size=batch_size,
+                shuffle=True,
+                drop_last=False,
+                num_workers=num_workers)
+
+            for input_nodes, output_nodes, blocks in tqdm.tqdm(dataloader):
+                block = blocks[0]
+
+                block = block.int().to(device)
+                h = x[input_nodes].to(device)
+                h = layer(block, h)
+                if l != len(self.layers) - 1:
+                    h = F.relu(h)
+                    h = self.dropout(h)
+
+                y[output_nodes] = h.cpu()
+
+            x = y
+        return y
+    
+
 class SAGE(nn.Module):
     def __init__(self,
                  in_feats,
