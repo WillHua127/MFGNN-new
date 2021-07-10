6989984c779f448d34daf703123f37eed05e21a2
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-07-10 06:37:13 -0400

    Update models.py

--- models.py
+++ models.py
@@ -13,10 +13,24 @@ class CPPooling(nn.Module):
         self.fc = FClayer(hidden, out_class)
         self.dropout = dropout
 
-    def forward(self, g, x, norm):
-        fea = F.relu(self.cp(g, x, norm))
+    def forward(self, g, x):
+        fea = F.relu(self.cp(g, x))
         fea = F.dropout(fea, self.dropout, training=self.training)
         out = self.fc(fea)
+        return out
+    
+    
+class TwoCPPooling(nn.Module):
+    def __init__(self, in_fea, hidden, out_class, rank1, rank2, dropout):
+        super(TwoCPPooling, self).__init__()
+        self.cp1 = CPlayer(in_fea, hidden, rank1)
+        self.cp2 = CPlayer(hidden, out_class, rank2)
+        self.dropout = dropout
+
+    def forward(self, g, x, norm):
+        fea = F.relu(self.cp1(g, x))
+        fea = F.dropout(fea, self.dropout, training=self.training)
+        out = self.cp2(g, fea)
         return out
 
     
