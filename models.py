ff90225d9f6630f2822ed8fb6200ffb876424dbc
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-07-12 23:47:16 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,36 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from layers import CPlayer, FClayer
-
+from dgl.nn.pytorch import GraphConv
     
 
-    
+
+
+class GCN(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 dropout):
+        super(GCN, self).__init__()
+        self.layers = nn.ModuleList()
+        # input layer
+        self.layers.append(GraphConv(in_feats, n_hidden, activation=F.relu))
+        # hidden layers
+        for i in range(n_layers - 1):
+            self.layers.append(GraphConv(n_hidden, n_hidden, activation=F.relu))
+        # output layer
+        self.layers.append(FClayer(n_hidden, n_classes))
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, g, features):
+        h = features
+        for i, layer in enumerate(self.layers):
+            if i != 0:
+                h = self.dropout(h)
+            h = layer(g, h)
+        return h
     
 class CPPooling(nn.Module):
     def __init__(self, in_fea, hidden, out_class, rank, dropout):
