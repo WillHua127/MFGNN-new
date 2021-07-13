bfec92c287e99a6c480f932e10a7b44cea158e68
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-07-12 23:53:06 -0400

    Update models.py

--- models.py
+++ models.py
@@ -8,27 +8,26 @@ from dgl.nn.pytorch import GraphConv
 
 class GCN(nn.Module):
     def __init__(self,
-                 in_feats,
-                 n_hidden,
-                 n_classes,
+                 in_fea,
+                 hidden,
+                 out_class,
                  n_layers,
                  dropout):
         super(GCN, self).__init__()
         self.layers = nn.ModuleList()
         # input layer
-        self.layers.append(GraphConv(in_feats, n_hidden, activation=F.relu))
+        self.layers.append(GraphConv(in_fea, hidden, activation=F.relu))
         # hidden layers
         for i in range(n_layers - 1):
-            self.layers.append(GraphConv(n_hidden, n_hidden, activation=F.relu))
+            self.layers.append(GraphConv(hidden, hidden, activation=F.relu))
         # output layer
-        self.layers.append(FClayer(n_hidden, n_classes))
-        self.dropout = nn.Dropout(p=dropout)
+        self.layers.append(FClayer(hidden, out_class))
 
     def forward(self, g, features):
         h = features
         for i, layer in enumerate(self.layers):
             if i != 0:
-                h = self.dropout(h)
+                h = F.dropout(h, self.dropout, training=self.training)
             h = layer(g, h)
         return h
     
