c5cddb9b0a11a7cbf7ac59cd9e16829c46acd3f7
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-08-10 13:04:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def main():
                         help='input batch size for training (default: 32)')
     parser.add_argument('--iters_per_epoch', type=int, default=50,
                         help='number of iterations per each epoch (default: 50)')
-    parser.add_argument('--epochs', type=int, default=1000,
+    parser.add_argument('--epochs', type=int, default=5000,
                         help='number of epochs to train (default: 350)')
     parser.add_argument('--lr', type=float, default=0.01,
                         help='learning rate (default: 0.01)')
@@ -134,7 +134,7 @@ def main():
     
     
     
-    patience = 50
+    patience = 100
     best_result = 0
     best_std = 0
     best_dropout = None
