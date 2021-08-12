57258006df643b0645592344ed469977a7330dc8
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-08-12 04:10:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -89,13 +89,13 @@ def main():
                         help='name of dataset (default: MUTAG)')
     parser.add_argument('--device', type=int, default=0,
                         help='which gpu to use if any (default: 0)')
-    parser.add_argument('--batch_size', type=int, default=32,
+    parser.add_argument('--batch_size', type=int, default=64,
                         help='input batch size for training (default: 32)')
     parser.add_argument('--iters_per_epoch', type=int, default=50,
                         help='number of iterations per each epoch (default: 50)')
     parser.add_argument('--epochs', type=int, default=5000,
                         help='number of epochs to train (default: 350)')
-    parser.add_argument('--lr', type=float, default=0.01,
+    parser.add_argument('--lr', type=float, default=0.05,
                         help='learning rate (default: 0.01)')
     parser.add_argument('--seed', type=int, default=42,
                         help='random seed for splitting the dataset into 10 (default: 0)')
