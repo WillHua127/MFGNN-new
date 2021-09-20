6262299baeee6d7edaff58165f8aa7dacea6de38
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-09-20 12:52:54 -0400

    Update models.py

--- models.py
+++ models.py
@@ -122,20 +122,24 @@ class GCN(nn.Module):
         return self.layers[-1](h)
     
 class SampleCPPooling(nn.Module):
-    def __init__(self, in_fea, hidden, out_class, rank, dropout):
+    def __init__(self, in_fea, hidden, out_class,dropout):
         super(SampleCPPooling, self).__init__()
-        self.cp = CPlayer(in_fea, hidden, rank)
-        self.fc = FClayer(hidden, out_class)
+        #self.cp = CPlayer(in_fea, hidden, 2*hidden)
+        #self.fc = FClayer(hidden, out_class)
+        #self.cp2 = CPlayer(hidden, out_class, 2)
+        self.conv1 = GraphConv(in_fea, hidden, 2*hidden)
+        self.conv2 = GraphConv(hidden, out_class, 2*out_class)
         self.dropout = dropout
 
     def forward(self, mfgs, x):
         h_dst = x[:mfgs[0].num_dst_nodes()]
-        h = self.cp(mfgs[0], (x, h_dst))
+        h = self.conv1(mfgs[0], (x, h_dst))
         h = F.relu(h)
         h = F.dropout(h, self.dropout, training=self.training)
-        out = self.fc(h)
+        h_dst = h[:mfgs[1].num_dst_nodes()]
+        out = self.conv2(mfgs[1], (h, h_dst))
+        #out = self.fc(h)
         return out
-            
         
 class CPPooling(nn.Module):
     def __init__(self, in_fea, hidden, out_class, rank, dropout):
