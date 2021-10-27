8c6aaf0490a2b3b3ba3466bb9fa67deaeaccda6f
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-10-27 11:19:24 -0400

    Update conv.py

--- conv.py
+++ conv.py
@@ -1,254 +1,197 @@
+from typing import List, Optional,Set
+from torch_geometric.typing import Adj, Size
+from torch_scatter.utils import broadcast
+
+
 import torch
-from torch_geometric.nn import MessagePassing
-import torch.nn.functional as F
-from torch_geometric.nn import global_mean_pool, global_add_pool
-from ogb.graphproppred.mol_encoder import AtomEncoder,BondEncoder
+from torch import Tensor
+
+from torch_geometric.utils.num_nodes import maybe_num_nodes
 from torch_geometric.utils import degree
+import torch.nn.functional as F
+from ogb.graphproppred.mol_encoder import BondEncoder
 
-import math
 
-### GIN convolution along the graph structure
-class GINConv(MessagePassing):
-    def __init__(self, emb_dim):
-        '''
-            emb_dim (int): node embedding dimensionality
-        '''
 
-        super(GINConv, self).__init__(aggr = "add")
+    
+class MessagePassing(torch.nn.Module):
 
-        self.mlp = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, emb_dim))
-        self.eps = torch.nn.Parameter(torch.Tensor([0]))
+    def __init__(self, aggr: Optional[str] = "add",
+                 flow: str = "target_to_source", node_dim: int = -2):
 
-        self.bond_encoder = BondEncoder(emb_dim = emb_dim)
+        super(MessagePassing, self).__init__()
 
-    def forward(self, x, edge_index, edge_attr):
-        edge_embedding = self.bond_encoder(edge_attr)
-        out = self.mlp((1 + self.eps) *x + self.propagate(edge_index, x=x, edge_attr=edge_embedding))
+        self.aggr = aggr
+        assert self.aggr in ['add', 'mean', 'max', None]
 
-        return out
+        self.flow = flow
+        assert self.flow in ['source_to_target', 'target_to_source']
 
-    def message(self, x_j, edge_attr):
-        return F.relu(x_j + edge_attr)
+        self.node_dim = node_dim
 
-    def update(self, aggr_out):
-        return aggr_out
 
-### GCN convolution along the graph structure
+    def __check_input__(self, edge_index, size):
+        the_size: List[Optional[int]] = [None, None]
+
+        if isinstance(edge_index, Tensor):
+            assert edge_index.dtype == torch.long
+            assert edge_index.dim() == 2
+            assert edge_index.size(0) == 2
+            if size is not None:
+                the_size[0] = size[0]
+                the_size[1] = size[1]
+            return the_size
+
+        raise ValueError(
+            ('`MessagePassing.propagate` only supports `torch.LongTensor` of '
+             'shape `[2, num_messages]` or `torch_sparse.SparseTensor` for '
+             'argument `edge_index`.'))
+
+    def __set_size__(self, size: List[Optional[int]], dim: int, src: Tensor):
+        the_size = size[dim]
+        if the_size is None:
+            size[dim] = src.size(self.node_dim)
+        elif the_size != src.size(self.node_dim):
+            raise ValueError(
+                (f'Encountered tensor with size {src.size(self.node_dim)} in '
+                 f'dimension {self.node_dim}, but expected size {the_size}.'))
+
+    def __lift__(self, src, edge_index, dim):
+        if isinstance(edge_index, Tensor):
+            index = edge_index[dim]
+            return src.index_select(self.node_dim, index)
+
+
+    def __collect__(self, edge_index, size, x):
+        i, j = (1, 0) if self.flow == 'source_to_target' else (0, 1)
+
+        dim = 0
+        data = x
+
+        if isinstance(data, (tuple, list)):
+            assert len(data) == 2
+            if isinstance(data[1 - dim], Tensor):
+                self.__set_size__(size, 1 - dim, data[1 - dim])
+            #data = data[dim]
+            data_sum = data[dim]
+            data_prod = data[dim+1]
+
+        #if isinstance(data, Tensor):
+        if isinstance(data_sum, Tensor) and isinstance(data_prod, Tensor):
+            self.__set_size__(size, dim, data_sum)
+            data_sum = self.__lift__(data_sum, edge_index, j)
+            data_prod = self.__lift__(data_prod, edge_index, j)
+        return data_sum, data_prod
+
+    def propagate(self, edge_index: Adj, x, size: Size = None, edge_attr = None, norm=None):
+
+        size = self.__check_input__(edge_index, size)
+
+        if isinstance(edge_index, Tensor) or not self.fuse:
+            x_sum,x_prod = self.__collect__(edge_index, size, x)
+            x_sum = self.message(x_sum, edge_attr, norm)
+            x_prod = self.message(x_prod, edge_attr, norm)
+            x_sum, x_prod = self.aggregate((x_sum, x_prod), edge_index[1],ptr=None)
+        return x_sum, x_prod
+
+    def message_simple(self, x_j: Tensor) -> Tensor:
+        return x_j
+
+
+    def aggregate(self, inputs: Tensor, index: Tensor,
+                  ptr: Optional[Tensor] = None,
+                  dim_size: Optional[int] = None) -> Tensor:
+        
+        return self.scatter_sum(inputs[0], index, dim=self.node_dim),self.scatter_product(inputs[1], index, dim=self.node_dim)
+
+    def update(self, inputs: Tensor) -> Tensor:
+        return inputs
+    
+    
+    def scatter_sum(self, src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+                out: Optional[torch.Tensor] = None,
+                dim_size: Optional[int] = None) -> torch.Tensor:
+        index = broadcast(index, src, dim)
+        if out is None:
+            size = list(src.size())
+            if dim_size is not None:
+                size[dim] = dim_size
+            elif index.numel() == 0:
+                size[dim] = 0
+            else:
+                size[dim] = int(index.max()) + 1
+            out = torch.zeros(size, dtype=src.dtype, device=src.device)
+            return out.scatter_add_(dim, index, src)
+        else:
+            return out.scatter_add_(dim, index, src)
+    
+    
+    def scatter_product(self, src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+            out: Optional[torch.Tensor] = None, dim_size: Optional[int] = None) -> torch.Tensor:
+    
+        index = broadcast(index, src, dim)
+        size = list(src.size())
+        if dim_size is not None:
+            size[dim] = dim_size
+        elif index.numel() == 0:
+            size[dim] = 0
+        else:
+            size[dim] = int(index.max()) + 1
+        out = torch.ones(size, dtype=src.dtype, device=src.device)
+        out.scatter_(dim, index, src, reduce='multiply')
+        return torch.nn.Parameter(out, requires_grad=True)
+    
+    
 class GCNConv(MessagePassing):
     def __init__(self, emb_dim):
         super(GCNConv, self).__init__(aggr='add')
 
-        self.linear = torch.nn.Linear(emb_dim, emb_dim)
+        self.w1 = torch.nn.Linear(emb_dim, emb_dim)
+        self.w2 = torch.nn.Linear(emb_dim, emb_dim)
+        self.v = torch.nn.Linear(emb_dim, emb_dim)
         self.root_emb = torch.nn.Embedding(1, emb_dim)
         self.bond_encoder = BondEncoder(emb_dim = emb_dim)
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        self.w1.reset_parameters()
+        self.w2.reset_parameters()
+        self.v.reset_parameters()
+        
+    def gcn_norm(self,edge_index, edge_weight=None, num_nodes=None, dtype=None):
+        num_nodes = maybe_num_nodes(edge_index, num_nodes)
+
+        if edge_weight is None:
+            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
+                                     device=edge_index.device)
+
+
+        row, col = edge_index[0], edge_index[1]
+        deg = scatter_add(edge_weight, col, dim=0, dim_size=num_nodes).float()
+        deg_inv_sqrt = deg.pow_(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+        return deg_inv_sqrt[row]  * deg_inv_sqrt[col]
 
     def forward(self, x, edge_index, edge_attr):
-        x = self.linear(x)
+        x_sum, x_prod = self.w1(x),self.w2(x)
+        #x_prod = self.w2(x)
         edge_embedding = self.bond_encoder(edge_attr)
 
         row, col = edge_index
 
-        #edge_weight = torch.ones((edge_index.size(1), ), device=edge_index.device)
-        deg = degree(row, x.size(0), dtype = x.dtype) + 1
+        deg = degree(row, x.size(0), dtype = x.dtype)
         deg_inv_sqrt = deg.pow(-0.5)
         deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
 
         norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
 
-        return self.propagate(edge_index, x=x, edge_attr = edge_embedding, norm=norm) + F.relu(x + self.root_emb.weight) * 1./deg.view(-1,1)
+
+        sum_agg, prod_agg = self.propagate(edge_index, x=(x_sum,x_prod), edge_attr = edge_embedding, norm=norm)
+
+        return self.v(prod_agg)+sum_agg + F.relu(x + self.root_emb.weight) * 1./deg.view(-1,1)
 
     def message(self, x_j, edge_attr, norm):
         return norm.view(-1, 1) * F.relu(x_j + edge_attr)
-
+    
     def update(self, aggr_out):
         return aggr_out
-
-
-### GNN to generate node embedding
-class GNN_node(torch.nn.Module):
-    """
-    Output:
-        node representations
-    """
-    def __init__(self, num_layer, emb_dim, device, drop_ratio = 0.5, JK = "last", residual = False, gnn_type = 'gin'):
-        '''
-            emb_dim (int): node embedding dimensionality
-            num_layer (int): number of GNN message passing layers
-        '''
-
-        super(GNN_node, self).__init__()
-        self.num_layer = num_layer
-        self.drop_ratio = drop_ratio
-        self.JK = JK
-        ### add residual connection or not
-        self.residual = residual
-        self.device = device
-
-        if self.num_layer < 2:
-            raise ValueError("Number of GNN layers must be greater than 1.")
-
-        self.atom_encoder = AtomEncoder(emb_dim)
-
-        ###List of GNNs
-        self.convs = torch.nn.ModuleList()
-        self.batch_norms = torch.nn.ModuleList()
-
-        for layer in range(num_layer):
-            if gnn_type == 'gin':
-                self.convs.append(GINConv(emb_dim))
-            elif gnn_type == 'gcn':
-                self.convs.append(GCNConv(emb_dim))
-            else:
-                raise ValueError('Undefined GNN type called {}'.format(gnn_type))
-
-            self.batch_norms.append(torch.nn.BatchNorm1d(emb_dim))
-
-    #def forward(self, batched_data):
-    def forward(self, x, edge_index, edge_attr=None):
-        #batched_data = batched_data.to_data_list()
-        #x, edge_index, edge_attr = x.to(self.device), edge_index.to(self.device), edge_attr.to(self.device)
-        #x = [data.x for data in batched_data]
-        #edge_index = [data.edge_index for data in batched_data]
-        #edge_attr = [data.edge_attr for data in batched_data]
-        ### computing input node embedding
-
-        #h_list = [self.atom_encoder(x)]
-        #h_list = [self.atom_encoder(x_i.to(self.device)) for x_i in x]
-        h_list = [self.atom_encoder(x_i) for x_i in x]
-        for layer in range(self.num_layer):
-
-            #h = self.convs[layer](h_list[layer], edge_index, edge_attr)
-            #if edge_attr is None:
-            #    h = [self.convs[layer](h_list[idx], edge_index[idx]) for idx in range(len(x))]
-            #else:
-                #h = [self.convs[layer](h_list[idx].to(self.device), edge_index[idx].to(self.device), edge_attr[idx].to(self.device)) for idx in range(len(x))]
-            h = [self.convs[layer](h_list[idx], edge_index[idx], edge_attr[idx]) for idx in range(len(x))]
-            #h = self.batch_norms[layer](h)
-            #h = [self.batch_norms[layer](h_i.to(self.device)) for h_i in h]
-            #h = [self.batch_norms[layer](h_i) for h_i in h]
-
-            if layer == self.num_layer - 1:
-                #remove relu for the last layer
-                #h = F.dropout(h, self.drop_ratio, training = self.training)
-                h = [F.dropout(h_i, self.drop_ratio, training = self.training) for h_i in h]
-            else:
-                #h = F.dropout(F.relu(h), self.drop_ratio, training = self.training)
-                h = [F.dropout(F.relu(h_i), self.drop_ratio, training = self.training) for h_i in h]
-
-            if self.residual:
-                h += h_list[layer]
-
-            h_list.append(h)
-
-        ### Different implementations of Jk-concat
-        if self.JK == "last":
-            node_representation = h_list[-1]
-        elif self.JK == "sum":
-            node_representation = 0
-            for layer in range(self.num_layer + 1):
-                node_representation += h_list[layer]
-
-        return node_representation
-
-
-### Virtual GNN to generate node embedding
-class GNN_node_Virtualnode(torch.nn.Module):
-    """
-    Output:
-        node representations
-    """
-    def __init__(self, num_layer, emb_dim, drop_ratio = 0.5, JK = "last", residual = False, gnn_type = 'gin'):
-        '''
-            emb_dim (int): node embedding dimensionality
-        '''
-
-        super(GNN_node_Virtualnode, self).__init__()
-        self.num_layer = num_layer
-        self.drop_ratio = drop_ratio
-        self.JK = JK
-        ### add residual connection or not
-        self.residual = residual
-
-        if self.num_layer < 2:
-            raise ValueError("Number of GNN layers must be greater than 1.")
-
-        self.atom_encoder = AtomEncoder(emb_dim)
-
-        ### set the initial virtual node embedding to 0.
-        self.virtualnode_embedding = torch.nn.Embedding(1, emb_dim)
-        torch.nn.init.constant_(self.virtualnode_embedding.weight.data, 0)
-
-        ### List of GNNs
-        self.convs = torch.nn.ModuleList()
-        ### batch norms applied to node embeddings
-        self.batch_norms = torch.nn.ModuleList()
-
-        ### List of MLPs to transform virtual node at every layer
-        self.mlp_virtualnode_list = torch.nn.ModuleList()
-
-        for layer in range(num_layer):
-            if gnn_type == 'gin':
-                self.convs.append(GINConv(emb_dim))
-            elif gnn_type == 'gcn':
-                self.convs.append(GCNConv(emb_dim))
-            else:
-                raise ValueError('Undefined GNN type called {}'.format(gnn_type))
-
-            self.batch_norms.append(torch.nn.BatchNorm1d(emb_dim))
-
-        for layer in range(num_layer - 1):
-            self.mlp_virtualnode_list.append(torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), \
-                                                    torch.nn.Linear(2*emb_dim, emb_dim), torch.nn.BatchNorm1d(emb_dim), torch.nn.ReLU()))
-
-
-    def forward(self, batched_data):
-
-        x, edge_index, edge_attr, batch = batched_data.x, batched_data.edge_index, batched_data.edge_attr, batched_data.batch
-
-        ### virtual node embeddings for graphs
-        virtualnode_embedding = self.virtualnode_embedding(torch.zeros(batch[-1].item() + 1).to(edge_index.dtype).to(edge_index.device))
-
-        h_list = [self.atom_encoder(x)]
-        for layer in range(self.num_layer):
-            ### add message from virtual nodes to graph nodes
-            h_list[layer] = h_list[layer] + virtualnode_embedding[batch]
-
-            ### Message passing among graph nodes
-            h = self.convs[layer](h_list[layer], edge_index, edge_attr)
-
-            h = self.batch_norms[layer](h)
-            if layer == self.num_layer - 1:
-                #remove relu for the last layer
-                h = F.dropout(h, self.drop_ratio, training = self.training)
-            else:
-                h = F.dropout(F.relu(h), self.drop_ratio, training = self.training)
-
-            if self.residual:
-                h = h + h_list[layer]
-
-            h_list.append(h)
-
-            ### update the virtual nodes
-            if layer < self.num_layer - 1:
-                ### add message from graph nodes to virtual nodes
-                virtualnode_embedding_temp = global_add_pool(h_list[layer], batch) + virtualnode_embedding
-                ### transform virtual nodes using MLP
-
-                if self.residual:
-                    virtualnode_embedding = virtualnode_embedding + F.dropout(self.mlp_virtualnode_list[layer](virtualnode_embedding_temp), self.drop_ratio, training = self.training)
-                else:
-                    virtualnode_embedding = F.dropout(self.mlp_virtualnode_list[layer](virtualnode_embedding_temp), self.drop_ratio, training = self.training)
-
-        ### Different implementations of Jk-concat
-        if self.JK == "last":
-            node_representation = h_list[-1]
-        elif self.JK == "sum":
-            node_representation = 0
-            for layer in range(self.num_layer + 1):
-                node_representation += h_list[layer]
-
-        return node_representation
-
-
-if __name__ == "__main__":
-    pass
