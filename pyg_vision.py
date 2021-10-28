9d4407844e2031436e37736f0a2a0359f6c48edc
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-10-28 07:54:21 -0400

    Update pyg_vision.py

--- pyg_vision.py
+++ pyg_vision.py
@@ -8,6 +8,7 @@ from torch.nn import Sequential, ReLU, Linear
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch_geometric.utils import degree
 from torch_geometric.datasets import GNNBenchmarkDataset
+import torch_geometric.transforms as T
 #from torch.utils.data import DataLoader
 from torch_geometric.nn import BatchNorm, global_add_pool#, GCNConv
 #from ogb.graphproppred.mol_encoder import AtomEncoder,BondEncoder
@@ -46,26 +47,26 @@ from torch_scatter import scatter_add
 
 argparser = argparse.ArgumentParser("multi-gpu training")
 argparser.add_argument('--epochs', type=int, default=300)
-argparser.add_argument('--hidden', type=int, default=75)
+argparser.add_argument('--hidden', type=int, default=5)
 argparser.add_argument('--emb', type=int, default=75)
 argparser.add_argument('--layers', type=int, default=4)
 argparser.add_argument('--lr', type=float, default=0.001)
 argparser.add_argument('--dropout', type=float, default=0.0)
-argparser.add_argument('--rank', type=int, default=75)
+argparser.add_argument('--rank', type=int, default=15)
 argparser.add_argument('--batch', type=int, default=1000)
-argparser.add_argument('--dataset', type=str, default="MNIST")
 args = argparser.parse_args()
 
-train_dataset = GNNBenchmarkDataset(osp.join('torch_geometric_data',args.dataset), name=args.dataset, split='train')
-val_dataset = GNNBenchmarkDataset(osp.join('torch_geometric_data',args.dataset), name=args.dataset, split='val')
-test_dataset = GNNBenchmarkDataset(osp.join('torch_geometric_data',args.dataset), name=args.dataset, split='test')
+train_dataset = GNNBenchmarkDataset(osp.join('data','MNIST'),name='MNIST',split='test', transform = T.Cartesian(cat=False))
+val_dataset = GNNBenchmarkDataset(osp.join('data','MNIST'),name='MNIST',split='test', transform = T.Cartesian(cat=False))
+test_dataset = GNNBenchmarkDataset(osp.join('data','MNIST'),name='MNIST',split='test', transform = T.Cartesian(cat=False))
+n_efeat = train_dataset[0].edge_attr.shape[1]
+n_nfeat = train_dataset[0].x.shape[1]
 n_class = train_dataset.num_classes
-print(train_dataset[0])
-    
+
 train_loader = DataLoader(train_dataset, batch_size=args.batch, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=args.batch)
 test_loader = DataLoader(test_dataset, batch_size=args.batch)
-
+    
 class MessagePassing(torch.nn.Module):
     special_args: Set[str] = {
         'edge_index', 'adj_t', 'edge_index_i', 'edge_index_j', 'size',
@@ -93,14 +94,6 @@ class MessagePassing(torch.nn.Module):
         self.__user_args__ = self.inspector.keys(
             ['message', 'aggregate', 'update']).difference(self.special_args)
 
-
-        # Hooks:
-#         self._propagate_forward_pre_hooks = OrderedDict()
-#         self._propagate_forward_hooks = OrderedDict()
-#         self._message_forward_pre_hooks = OrderedDict()
-#         self._message_forward_hooks = OrderedDict()
-#         self._aggregate_forward_pre_hooks = OrderedDict()
-#         self._aggregate_forward_hooks = OrderedDict()
 
     def __check_input__(self, edge_index, size):
         the_size: List[Optional[int]] = [None, None]
@@ -137,16 +130,12 @@ class MessagePassing(torch.nn.Module):
     def __collect__(self, args, edge_index, size, x):
         i, j = (1, 0) if self.flow == 'source_to_target' else (0, 1)
 
-#        out = {}
         for arg in args:
             if arg[-2:] not in ['_i', '_j']:
-                #data_sum = x[0]
-                #data_prod = x[1]
-                #out[arg] = kwargs.get(arg, Parameter.empty)
+
                 pass
             else:
                 dim = 0 if arg[-2:] == '_j' else 1
-                #data = kwargs.get(arg[:-2], Parameter.empty)
                 data = x
 
                 if isinstance(data, (tuple, list)):
@@ -157,78 +146,22 @@ class MessagePassing(torch.nn.Module):
                     data_sum = data[dim]
                     data_prod = data[dim+1]
 
-                #if isinstance(data, Tensor):
                 if isinstance(data_sum, Tensor) and isinstance(data_prod, Tensor):
-    #                     self.__set_size__(size, dim, data)
-    #                     data = self.__lift__(data, edge_index,
-    #                                          j if arg[-2:] == '_j' else i)
+
                     self.__set_size__(size, dim, data_sum)
                     data_sum = self.__lift__(data_sum, edge_index, j if arg[-2:] == '_j' else i)
                     data_prod = self.__lift__(data_prod, edge_index, j if arg[-2:] == '_j' else i)
 
-#                out[arg] = data
-
-#         if isinstance(edge_index, Tensor):
-#             out['adj_t'] = None
-#             out['edge_index'] = edge_index
-#             out['edge_index_i'] = edge_index[i]
-#             out['edge_index_j'] = edge_index[j]
-#             out['ptr'] = None
-
-#         out['index'] = out['edge_index_i']
-#         out['size'] = size
-#         out['size_i'] = size[1] or size[0]
-#         out['size_j'] = size[0] or size[1]
-#         out['dim_size'] = out['size_i']
-
-        #return out
         return data_sum, data_prod
 
     def propagate(self, edge_index: Adj, x, size: Size = None, edge_attr = None, norm=None):
-#         for hook in self._propagate_forward_pre_hooks.values():
-#             res = hook(self, (edge_index, size, kwargs))
-#             if res is not None:
-#                 edge_index, size, kwargs = res
-
         size = self.__check_input__(edge_index, size)
 
         if isinstance(edge_index, Tensor) or not self.fuse:
-#             coll_dict = self.__collect__(self.__user_args__, edge_index, size,
-#                                          kwargs)
-#             msg_kwargs = self.inspector.distribute('message', coll_dict)
             x_sum,x_prod = self.__collect__(self.__user_args__, edge_index, size, x)
-#             for hook in self._message_forward_pre_hooks.values():
-#                 res = hook(self, (msg_kwargs, ))
-#                 if res is not None:
-#                     msg_kwargs = res[0] if isinstance(res, tuple) else res
-            #x_sum = self.message_simple(x_sum)
             x_sum = self.message(x_sum, edge_attr, norm)
             x_prod = self.message(x_prod, edge_attr, norm)
-#             x_sum = self.message(x_sum, edge_attr)
-#             x_prod = self.message(x_prod, edge_attr)
-#             for hook in self._message_forward_hooks.values():
-#                 res = hook(self, (msg_kwargs, ), out)
-#                 if res is not None:
-#                     out = res
-
-#            aggr_kwargs = self.inspector.distribute('aggregate', coll_dict)
-#             for hook in self._aggregate_forward_pre_hooks.values():
-#                 res = hook(self, (aggr_kwargs, ))
-#                 if res is not None:
-#                     aggr_kwargs = res[0] if isinstance(res, tuple) else res
             x_sum, x_prod = self.aggregate((x_sum, x_prod), edge_index[1],ptr=None)
-#             for hook in self._aggregate_forward_hooks.values():
-#                 res = hook(self, (aggr_kwargs, ), out)
-#                 if res is not None:
-#                     out = res
-
-#             update_kwargs = self.inspector.distribute('update', coll_dict)
-#             out = self.update(out, **update_kwargs)
-
-#         for hook in self._propagate_forward_hooks.values():
-#             res = hook(self, (edge_index, size, kwargs), out)
-#             if res is not None:
-#                 out = res
 
         return x_sum, x_prod
 
@@ -276,17 +209,6 @@ class MessagePassing(torch.nn.Module):
         else:
             size[dim] = int(index.max()) + 1
         out = torch.ones(size, dtype=src.dtype, device=src.device)
-        #return scatter_add_(dim, index, src)
-        #for i in range(index.size(0)):
-        #    for j in range(index.size(1)):
-        #        replace_idx = index[i][j]
-        #        if dim == -2:
-        #            out[replace_idx][j] = out[replace_idx][j]+src[i][j]
-        #        elif dim == -1:
-        #            out[i][replace_index] = out[i][replace_index]+src[i][j]
-        #for i in range(out.shape[0]):
-        #    out[i]=torch.sum(src[index==i], dim=0)
-        #with torch.no_grad():
         out.scatter_(dim, index, src, reduce='multiply')
         return torch.nn.Parameter(out, requires_grad=True)
     
@@ -297,7 +219,7 @@ class GCNConv(MessagePassing):
 
         self.w1 = torch.nn.Linear(emb_dim, hidden_dim)
         self.w2 = torch.nn.Linear(emb_dim, rank_dim)
-        self.v = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.v = torch.nn.Linear(rank_dim, hidden_dim)
         self.root_emb = torch.nn.Embedding(1, hidden_dim)
         #self.bond_encoder = BondEncoder(emb_dim = emb_dim)
         self.reset_parameters()
@@ -347,17 +269,13 @@ class GCNConv(MessagePassing):
         deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
 
         norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
-        #edge_index, edge_attr = self.gcn_norm(edge_index,edge_weight=edge_attr)
         sum_agg, prod_agg = self.propagate(edge_index, x=(x_sum,x_prod), edge_attr = edge_attr, norm=norm)
 
         return self.v(prod_agg)+sum_agg + F.relu(x + self.root_emb.weight) * 1./deg.view(-1,1)
 
     def message(self, x_j, edge_attr, norm):
         return norm.view(-1, 1) * F.relu(x_j + edge_attr)
-#    def message(self, x_j, edge_attr):
-#        return edge_attr.view(-1, ) * x_j
 
-    def update(self, aggr_out):
         return aggr_out
 
 class Net(torch.nn.Module):
@@ -369,8 +287,8 @@ class Net(torch.nn.Module):
                  dropout):
         super(Net, self).__init__()
 
-        self.node_emb = Embedding(21, emb_dim)
-        self.edge_emb = Embedding(4, emb_dim)
+        self.node_emb = torch.nn.Linear(n_nfeat, emb_dim)
+        self.edge_emb = torch.nn.Linear(n_efeat, emb_dim)
         self.n_layers = n_layers
         self.dropout = dropout
 
@@ -379,28 +297,30 @@ class Net(torch.nn.Module):
 
         self.convs = ModuleList()
         self.batch_norms = ModuleList()
+        #self.convs.append(GCNConv(emb_dim=n_nfeat,hidden_dim=emb_dim,rank_dim=emb_dim))
+        #self.batch_norms.append(BatchNorm(emb_dim))
         for _ in range(n_layers):
             #conv = PNAConv(in_channels=75, out_channels=75,
             #               aggregators=aggregators, scalers=scalers, deg=deg,
             #               edge_dim=50, towers=5, pre_layers=1, post_layers=1,
             #               divide_input=False)
-            conv = GCNConv(emb_dim=emb_dim, hidden_dim=hidden_dim, rank_dim=rank_dim)
+            #conv = GCNConv(emb_dim=emb_dim, hidden_dim=hidden_dim, rank_dim=rank_dim)
+            conv = GCNConv(emb_dim=emb_dim, hidden_dim=emb_dim, rank_dim=emb_dim)
             self.convs.append(conv)
-            self.batch_norms.append(BatchNorm(hidden_dim))
+            self.batch_norms.append(BatchNorm(emb_dim))
 
-        self.mlp = Sequential(Linear(hidden_dim, 50), ReLU(), Linear(50, 25), ReLU(),
+        self.mlp = Sequential(Linear(emb_dim, 50), ReLU(), Linear(50, 25), ReLU(),
                               Linear(25, n_class))
 
     def forward(self, x, edge_index, edge_attr, batch):
-        x = self.node_emb(x.squeeze())
+        x = self.node_emb(x)
         edge_attr = self.edge_emb(edge_attr)
-
         for layer in range(self.n_layers):
             if layer == self.n_layers - 1:
-                x = F.relu(self.batch_norms[layer](self.convs[layer](x, edge_index, edge_attr)))
+                x = F.relu(self.batch_norms[layer](self.convs[layer](x, edge_index,edge_attr)))
             else:
                 #x = F.relu(batch_norm[layer](conv[layer](x, edge_index, edge_attr)))
-                x = F.dropout(F.relu(self.batch_norms[layer](self.convs[layer](x, edge_index, edge_attr))), self.dropout, training = self.training)
+                x = F.dropout(F.relu(self.batch_norms[layer](self.convs[layer](x, edge_index,edge_attr))), self.dropout, training = self.training)
 
         #for conv, batch_norm in zip(self.convs, self.batch_norms):
         #    x = F.relu(batch_norm(conv(x, edge_index, edge_attr)))
@@ -417,7 +337,8 @@ def train(model, epoch, device):
         data = data.to(device)
         optimizer.zero_grad()
         out = model(data.x, data.edge_index, data.edge_attr, data.batch)
-        loss = (out.squeeze() - data.y).abs().mean()
+        out = F.log_softmax(out, dim=1)
+        loss =  F.nll_loss(out, data.y)
         loss.backward()
         total_loss += loss.item() * data.num_graphs
         optimizer.step()
@@ -428,12 +349,26 @@ def train(model, epoch, device):
 def test(model, loader, device):
     model.eval()
 
-    total_error = 0
+    correct = 0
     for data in loader:
         data = data.to(device)
         out = model(data.x, data.edge_index, data.edge_attr, data.batch)
-        total_error += (out.squeeze() - data.y).abs().sum().item()
-    return total_error / len(loader.dataset)
+        out = F.log_softmax(out, dim=1).max(1)[1]
+        correct += out.eq(data.y).sum().item()
+    return correct / len(test_dataset)
+
+@torch.no_grad()
+def eval(model, loader, device):
+    model.eval()
+
+    correct = 0
+    for data in loader:
+        data = data.to(device)
+        out = model(data.x, data.edge_index, data.edge_attr, data.batch)
+        out = F.log_softmax(out, dim=1).max(1)[1]
+        correct += out.eq(data.y).sum().item()
+    return correct / len(val_dataset)
+
 
 
 if __name__ == '__main__':
@@ -448,15 +383,15 @@ if __name__ == '__main__':
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=20,
                                   min_lr=0.00001)
 
-    best_eval = 1000000
-    best_test = 1000000
+    best_eval = 0
+    best_test = 0
     for epoch in range(1, args.epochs):
         loss = train(model, epoch, device)
-        val_mae = test(model, val_loader, device)
+        val_mae = eval(model, val_loader, device)
         test_mae = test(model, test_loader, device)
         if val_mae < best_eval:
             best_eval = val_mae
             best_test = test_mae
         scheduler.step(val_mae)
-        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Val: {val_mae:.4f}, '
-              f'Test: {test_mae:.4f}', f'Best val: {best_eval:.4f}', f'Best test: {best_test:.4f}')
+        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Val: {val_acc:.4f}, '
+              f'Test: {test_acc:.4f}', f'Best val: {best_eval:.4f}', f'Best test: {best_test:.4f}')
