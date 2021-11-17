299d46c81898027a4a0e78d3fd9c1ac93efd296c
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-11-17 11:45:16 -0500

    Update pyg_zinc.py

--- pyg_zinc.py
+++ pyg_zinc.py
@@ -222,9 +222,10 @@ class MessagePassing(torch.nn.Module):
 #                 res = hook(self, (msg_kwargs, ))
 #                 if res is not None:
 #                     msg_kwargs = res[0] if isinstance(res, tuple) else res
-            #x_sum = self.message_simple(x_sum)
-            x_sum = self.message(x_sum, edge_attr, norm)
-            x_prod = self.message(x_prod, edge_attr, norm)
+            x_sum = self.message_simple(x_sum)
+            x_prod = self.message_simple(x_prod)
+            #x_sum = self.message(x_sum, edge_attr, norm)
+            #x_prod = self.message(x_prod, edge_attr, norm)
 #             x_sum = self.message(x_sum, edge_attr)
 #             x_prod = self.message(x_prod, edge_attr)
 #             for hook in self._message_forward_hooks.values():
