ba4bef870c88c148d1edcedc2741e644dae10b73
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2021-11-19 03:34:19 -0500

    Update pyg_node.py

--- pyg_node.py
+++ pyg_node.py
@@ -47,7 +47,7 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=5,
+parser.add_argument('--epochs', type=int, default=5000,
                     help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.05,
                     help='Initial learning rate.')
