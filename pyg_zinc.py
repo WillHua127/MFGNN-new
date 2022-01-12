3227e1a694ca2c194c8fa1d9b2ecd886c3b40e62
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2022-01-12 18:29:38 -0500

    Update pyg_zinc.py

--- pyg_zinc.py
+++ pyg_zinc.py
@@ -56,7 +56,7 @@ argparser.add_argument('--dropout', type=float, default=0.0)
 argparser.add_argument('--rank', type=int, default=100)
 argparser.add_argument('--batch', type=int, default=1000)
 args = argparser.parse_args()
-args.hidden,args.rank = args.emb,args.emb
+#args.hidden,args.rank = args.emb,args.emb
 
 train_dataset = ZINC(osp.join('torch_geometric_data','zinc'), subset=True, split='train')
 val_dataset = ZINC(osp.join('torch_geometric_data','zinc'), subset=True, split='val')
